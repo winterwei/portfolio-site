18b59dec6d56e9a9893ddefeee7af77696a4028c
Winter Wei <winterwei@gmail.com>
2014-08-21 20:14:36 -0400

    keep stylin

--- page-home.php
+++ page-home.php
@@ -40,24 +40,27 @@ get_header();  ?>
 <section class="portfolio main clearfix">
   <div class="innerWrapper clearfix">
     <div id="portfolio">
-      <?php 
-        $portfolioQuery = new WP_Query(
-            array(
-              'posts_per_page' => 4,
-              'post_type' => 'portfolio'
-              )
-          ); ?>
-      <?php while ($portfolioQuery->have_posts()) : $portfolioQuery->the_post(); ?>
-        <figure class="col2">
-          <?php the_post_thumbnail('large'); ?>
-        </figure>
-        <a href="<?php the_permalink(); ?>">View Project</a>
-        <figcaption>
-          <h4><?php the_field('portfolio_title') ?></h4>
-          <p><?php the_field('short_description') ?></p>
-        </figcaption>
-      <?php endwhile; // end the loop?>
-      <?php wp_reset_postdata(); ?>
+      <h2>Things I've done</h2>
+        <?php 
+          $portfolioQuery = new WP_Query(
+              array(
+                'posts_per_page' => 4,
+                'post_type' => 'portfolio'
+                )
+            ); ?>
+        <?php while ($portfolioQuery->have_posts()) : $portfolioQuery->the_post(); ?>
+          <div class="col2">
+            <figure class="img-hover">
+              <?php the_post_thumbnail('large'); ?>
+              <a href="<?php the_permalink(); ?>">View Project</a>
+            </figure>
+            <figcaption>
+              <h4><?php the_field('portfolio_title') ?></h4>
+              <p><?php the_field('short_description') ?></p>
+            </figcaption>
+          </div>
+        <?php endwhile; // end the loop?>
+        <?php wp_reset_postdata(); ?>
     </div>
   </div>
 </section>
@@ -91,6 +94,9 @@ get_header();  ?>
     <div id="contact">
       <div class="contact-msg">
         <p><?php the_field('contact_message') ?></p>
+      </div>
+      <div class="contact-form">
+        <?php echo do_shortcode( '[contact-form-7 id="44" title="Contact form 1"]' ); ?>
       </div>
     </div>
   </div>
